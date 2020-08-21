d93c6f33d4b69e3c45bd601b0f0f0fb2b1f3a5c0
Wolfgang Hennerbichler <wogri@wogri.com>
2020-08-21 22:18:20 +0200

    temperature code update, now understands json and the master-struct

--- temperature/go.mod
+++ temperature/go.mod
@@ -1,3 +1,6 @@
 module github.com/wogri/bhive/temperature
 
-require github.com/yryz/ds18b20 v0.0.0-20200527154408-4a8f84bb82d4
+require (
+	github.com/wogri/bbox/structs/temperature v0.0.0-20200821195800-6ac29f1ecf94
+	github.com/yryz/ds18b20 v0.0.0-20200527154408-4a8f84bb82d4
+)
--- temperature/main.go
+++ temperature/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-  "flag"
+	"log"
+	"net"
+	"os"
 
+	"encoding/json"
+	"github.com/wogri/bbox/structs/temperature"
 	"github.com/yryz/ds18b20"
 )
 
-var serverAddr= flag.String("server_addr", "http://machine.intranet.wogri.com:8333/temperature", "HTTP server port")
+var serverAddr = flag.String("server_addr", "http://machine.intranet.wogri.com:8333/temperature", "HTTP server port")
 var debug = flag.Bool("debug", false, "debug mode")
+
+func getMacAddr() (string, error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	a := interfaces[1].HardwareAddr.String()
+	if a != "" {
+		return a, nil
+	}
+	return "", nil
+}
 
 func main() {
 	flag.Parse()
@@ -17,12 +34,26 @@ func main() {
 		panic(err)
 	}
 
+	mac, err := getMacAddr()
+	log.Println("mac: ", mac)
+	if err != nil {
+		log.Println(err)
+		os.Exit(2)
+	}
+
 	fmt.Printf("sensor IDs: %v\n", sensors)
 
 	for _, sensor := range sensors {
-		t, err := ds18b20.Temperature(sensor)
-		if err == nil {
-			fmt.Printf("sensor: %s temperature: %.2f°C\n", sensor, t)
+		measured_temperature, err := ds18b20.Temperature(sensor)
+		if err != nil {
+			log.Println(err)
+			os.Exit(3)
 		}
+		t := temperature.Temperature{
+			Temperature: measured_temperature,
+			BBoxID:      mac,
+			SensorID:    sensor}
+		payload, err := json.Marshal(t)
+		fmt.Printf(string(payload))
 	}
 }
