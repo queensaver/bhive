1109523122608d05bf00b442ab5deec65f24ea65
Wolfgang Hennerbichler <wogri@github.com>
2021-04-05 09:09:05 +0200

    make temperature measurement its own package

---
+++ packages/temperature/go.mod
@@ -0,0 +1,8 @@
+module github.com/wogri/bhive/packages/temperature
+
+go 1.16
+
+require (
+	github.com/wogri/bbox/packages/temperature v0.0.0-20210402053043-1d5fc3c63f9b
+	github.com/yryz/ds18b20 v0.0.0-20200527154408-4a8f84bb82d4
+)
---
+++ packages/temperature/temperature.go
@@ -0,0 +1,25 @@
+package temperature
+
+import (
+	"github.com/wogri/bbox/packages/temperature"
+	"github.com/yryz/ds18b20"
+)
+
+func GetTemperature(mac string) (*temperature.Temperature, error) {
+	sensors, err := ds18b20.Sensors()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, sensor := range sensors {
+		measured_temperature, err := ds18b20.Temperature(sensor)
+		if err != nil {
+			return nil, err
+		}
+		return &temperature.Temperature{
+			Temperature: measured_temperature,
+			BBoxID:      mac,
+			SensorID:    sensor}, nil
+	}
+	return nil, nil
+}
