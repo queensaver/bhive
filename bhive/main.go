6a8da5bd9d9dad50cfc66940cbe8a333c5920f31
Wolfgang Hennerbichler <wogri@github.com>
2021-04-05 10:21:40 +0200

    temp logicc

--- bhive/main.go
+++ bhive/main.go
@@ -1,7 +1,16 @@
 package main
 
 import (
-  _	"embed"
+	"bytes"
+	_ "embed"
+	"encoding/json"
+	"flag"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+
+	"github.com/wogri/bhive/packages/temperature"
 )
 
 //go:embed scale.py
@@ -10,7 +19,52 @@ var pyScale []byte
 //go:embed hx711.py
 var pyHx711 []byte
 
+var serverAddr = flag.String("server_addr", "http://192.168.233.1:8333/temperature", "HTTP server port")
+
+func getMacAddr() (string, error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	a := interfaces[1].HardwareAddr.String()
+	if a != "" {
+		r := strings.Replace(a, ":", "", -1)
+		return r, nil
+	}
+	return "", nil
+}
+
+func post(t temperature.Temperature) error {
+	j, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", *serverAddr, bytes.NewBuffer(j))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return (err)
+	}
+	defer resp.Body.Close()
+
+	if resp.Status != "200" {
+		log.Println("Got http return code ", resp.Status)
+	}
+	return nil
+}
+
 func main() {
+	mac, err := getMacAddr()
+	if err != nil {
+		log.Fatal(err)
+	}
+	temperature, err := temperature.GetTemperature(mac)
+	if err != nil {
+		log.Println("Error getting temperature: ", err)
+	}
+	post(temperature)
 	print(string(pyScale))
 	print(string(pyHx711))
 }
