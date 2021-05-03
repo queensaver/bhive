16855bc21f2a0b339847131c9af38a7b23edba89
Wolfgang Hennerbichler <wogri@wogri.com>
2021-05-03 12:00:36 +0200

    we are called queensaver

--- ansible/install.sh
+++ ansible/install.sh
@@ -9,7 +9,7 @@ sudo apt install -y ansible git
 if [ -e bhive ]; then
   git -C bhive/ pull
 else
-  git clone https://github.com/btelemetry/bhive.git
+  git clone https://github.com/queensaver/bhive.git
 fi
 
 ansible-playbook bhive/ansible/bhive.yml
